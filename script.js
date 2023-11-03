31f6bf181510f71f02bbb2cff6626f651e216cba
muhammad hanif <116032896+softhanni@users.noreply.github.com>
2023-11-03 16:14:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var a1 = ('1st assignment')
+document.write (a1)
+document.write("<br>");
+
+var x = 50;
+var y = 10;
+
+
+ //(x+y) ye 1 expression hy
+document.write (x+y)
+document.write("<br>");
+//additon operation x+y = 50+10=60 
+
+document.write (x-y)
+document.write("<br>");
+//minus operation x-y = 50-10=40 
+
+document.write (x/y)
+document.write("<br>");
+// division operation x/y = 50/10=5
+
+document.write (x*y)
+document.write("<br>");
+// Multiplication operation x*y = 50*10=500 
+
+document.write (x%y)
+document.write("<br>");
+//modules or reminder operation x%y = 50+10=0 
+
+var a2 = ('2nd assignment')
+document.write (a2)
+document.write("<br>");
+
+
+//Assignment 2nd | operator precedence
+
+var precedence =10-4/2*5+9;
+/*wese JS left to right kam krti hy lekin mathemetics me BODMASS rule apply karti hy jese nature k hisaab se pehly minus hona chaiye par yahan pr BODMASS rule apply hora hy is wajah se pehly division ya multiplication hogi us k bad ,imus plus*/
+
+document.write (precedence)
+document.write("<br>");
+//Assignment 3rd | Operators
+var a3 = ('3rd assignment')
+document.write (a3)
+document.write("<br>");
+var x = 5
+document.write (x+=3)
+document.write("<br>");
+document.write (x-=2)
+document.write("<br>");
+document.write (x*=4)
+document.write("<br>");
+document.write (x/=4)
+
+document.write("<br>");
+//Assignment 4th | Increment and Decrement Operators
+var a4 = ('4th assignment')
+document.write (a4)
+var x= 10
+document.write (++x)
+document.write("<br>");
+document.write (--x)
+
